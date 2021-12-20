5851baeb21d375c5d575a943a8d3eeb80edde73f
Sergiu Botnaruc <botnaruc@yahoo.com>
2021-12-20 17:04:28 +0200

    docs

--- java-java/core/src/main/java/org/stabila/java/core/exceptions/ContractCreateException.java
+++
@@ -1,8 +0,0 @@
-package org.stabila.java.core.exceptions;
-
-public class ContractCreateException extends Exception {
-    
-    public ContractCreateException(String message) {
-        super(message);
-    }
-}
